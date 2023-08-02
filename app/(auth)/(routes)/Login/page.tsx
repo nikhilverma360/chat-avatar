e6f59a2cf681f41cee8edefb0fc24c8377b41e80
Nikhil Verma <nv5475609@gmail.com>
2023-08-02 12:57:37 +0530

    Clerk Auth ✍️

--- app/(auth)/(routes)/Login/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className='text-3xl text-red-600'>Hello Chat Avatar Auth</h1> 
-    </div>
-  )
-}
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <h1 className='text-3xl text-red-600'>Hello Chat Avatar Root</h1> 
+      <UserButton afterSignOutUrl="/" />
     </div>
-  )
+  );
 }
