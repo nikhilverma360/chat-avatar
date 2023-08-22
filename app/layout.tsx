6abbaf7c4145010ae7ab97ded657069b018dba36
Nikhil Verma <nv5475609@gmail.com>
2023-08-22 13:31:13 +0530

    navbar 🧭

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,33 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ['latin'] })
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Chat Avatar',
-  description: 'Chat with your favorite Avatars',
-}
+  title: "Companion.AI",
+  description: "Your customized companion.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn("bg-secondary", inter.className)}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
